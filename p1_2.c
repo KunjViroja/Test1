e19633c84af341174319d0a64d484b2d35e09112
KunjViroja <152464988+KunjViroja@users.noreply.github.com>
2023-11-30 09:54:32 +0530

    Add files via upload

---
+++ p1_2.c
@@ -0,0 +1,10 @@
+main()
+{
+printf("\n\n\t%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1);
+printf("\n\t%c \"If you are resisting something, you are feeding it.                  %c",4,4);
+printf("\n\t%c      Any energy you fight, you are feeding it.                        %c",3,3);
+printf("\n\t%c              If you are pushing somthing away,                        %c",1,1);
+printf("\n\t%c                      You are inviting it to stay.\" by Michael Singer. %c",4,4);
+printf("\n\t%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c\n\n\n",1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1,4,3,1);
+printf("\n Kunj_23IT138");
+}
